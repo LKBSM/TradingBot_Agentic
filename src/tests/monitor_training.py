66ffa8d90e570beb5921f02bb51b1e5580dfa18c
Loukmane Bessam <loukmanebessam@gmail.com>
2025-12-13 21:25:52 -0500

    Add missing TradeLogger module

---
+++ src/tests/monitor_training.py
@@ -0,0 +1,9 @@
+﻿class TradeLogger:
+    def __init__(self):
+        self.trades = []
+        
+    def log_trade(self, trade_data):
+        self.trades.append(trade_data)
+        
+    def log_episode_summary(self, summary_data):
+        pass
